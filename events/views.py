dbd9709d65240068ec7157bf0b7949ad0a4fdd06
Jalen Hobbs <jalenhobbs@unomaha.edu>
2026-03-12 20:15:53 -0500

    Modified views.py to support additional views for the calendar.

--- events/views.py
+++ events/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Event
+from .forms import EventForm
 
-# Create your views here.
-from django.http import HttpResponse
 
-def event_list(request):
-	return HttpResponse("Event list test")
+class EventListView(ListView):
+    model = Event
+    template_name = "events/event_list.html"
+    context_object_name = "events"
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = "events/event_detail.html"
+    context_object_name = "event"
+
+
+class EventCreateView(CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = "events/event_form.html"
+
+
+class EventUpdateView(UpdateView):
+    model = Event
+    form_class = EventForm
+    template_name = "events/event_form.html"
+
+
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = "events/event_confirm_delete.html"
+    success_url = reverse_lazy("event_list")
