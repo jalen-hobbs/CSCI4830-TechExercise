a17f4933b9caaaa316f40d503844053abffa14bb
Jalen Hobbs <jalenhobbs@unomaha.edu>
2026-03-12 19:50:13 -0500

    Created basic structure for events app in Django

--- tech_exercise/urls.py
+++ tech_exercise/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hello_world_app.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello),
+    path('', include('events.urls')),
 ]
