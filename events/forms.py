322f40eb5cacd496559b280a2dcba374fc6814aa
Jalen Hobbs <jalenhobbs@unomaha.edu>
2026-03-12 20:55:21 -0500

    Removed ability to change status as this is handled automatically now.

--- events/forms.py
+++ events/forms.py
@@ -4,10 +4,10 @@ from .models import Event
 
 class EventForm(forms.ModelForm):
     start_datetime = forms.DateTimeField(
-        widget=forms.DateTimeInput(attrs={"type": "datetime-local"})
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}, input_formats=["%Y-%m-%dT%H:%M"])
     )
     end_datetime = forms.DateTimeField(
-        widget=forms.DateTimeInput(attrs={"type": "datetime-local"})
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}, input_formats=["%Y-%m-%dT%H:%M"])
     )
 
     class Meta:
@@ -19,5 +19,5 @@ class EventForm(forms.ModelForm):
             "end_datetime",
             "location",
             "category",
-            "status",
+            "is_cancelled",
         ]
