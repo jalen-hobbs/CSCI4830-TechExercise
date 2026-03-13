109d24eef56ba8f1c34e4d29b6edba0cdd33ab3a
Jalen Hobbs <jalenhobbs@unomaha.edu>
2026-03-12 19:52:51 -0500

    Renamed project dir to calendarplanner

--- calendarplanner/wsgi.py
+++ calendarplanner/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tech_exercise project.
+WSGI config for calendarplanner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_exercise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendarplanner.settings')
 
 application = get_wsgi_application()
