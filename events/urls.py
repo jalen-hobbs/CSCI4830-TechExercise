d108026645a90ca83df89752e50f404532b60382
Jalen Hobbs <jalenhobbs@unomaha.edu>
2026-03-12 20:19:11 -0500

    Changed urls.py to support updated views

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from . import views
+from .views import (
+    EventListView,
+    EventDetailView,
+    EventCreateView,
+    EventUpdateView,
+    EventDeleteView,
+)
 
 urlpatterns = [
-    path('', views.event_list, name='event_list'),
+    path("", EventListView.as_view(), name="event_list"),
+    path("event/<int:pk>/", EventDetailView.as_view(), name="event_detail"),
+    path("event/add/", EventCreateView.as_view(), name="event_create"),
+    path("event/<int:pk>/edit/", EventUpdateView.as_view(), name="event_edit"),
+    path("event/<int:pk>/delete/", EventDeleteView.as_view(), name="event_delete"),
 ]
